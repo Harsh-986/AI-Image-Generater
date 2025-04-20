65a9c994237a7ea346884f2db5dd012eae647661
Harsh-986 <hy025u@gmail.com>
2025-04-20 21:53:17 +0530

    Day 3

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 
+import { preview } from '../assets'
+import { getRandomPrompt } from '../utils'
+import {Loader, FormField} from '../components'
 const CreatePost = () => {
+  const navigate = useNavigate()
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: ''
+  })
+  const [generatingImg, setGeneratingImg] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = () => {}
+
+  const handleChange = (e) => {}
+
+  const handleSupriseMe = () => {}
+
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>The Community Showcase</h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w-[500px]'>Create imaginative and visually stunning images generated through DALL-E AI and share them with community</p>
+      </div>
+
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
+          <FormField
+            LabelName="Your Name"
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            LabelName="prompt"
+            type="text"
+            name="prompt"
+            placeholder="an armchair in the shape of an avocado"
+            value={form.name}
+            handleChange={handleChange}
+            handleSupriseMe={handleSupriseMe}
+          />
+        </div>
+      </form>
+    </section>
   )
 }
 
